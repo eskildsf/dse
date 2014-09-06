65644769f351970642b30d5d331cac0132dd0310
Eskild Schroll-Fleischer <esf@studerende.dk>
2014-09-06 14:28:38 +0200

    Price shown with character count A calculation of the expected cost of the text message is now also shown below the character count in Questionnaire/Survey

--- static/questionnaire/js/jquery.character-counter.js
+++ static/questionnaire/js/jquery.character-counter.js
@@ -3,7 +3,16 @@
 	
 		function calculate(obj){
 			var count = $(obj).val().length;
-			$(obj).next().html('Characters: ' + count);
+                        // The first 160 characters cost 0.6 kr.
+                        // Every additional 140 characters cost 0.3 kr.
+                        var price = 0;
+                        if ( count > 0 ) {
+                        price = price + 0.6;
+                        }
+                        if ( count > 160 ) {
+                        price = price + Math.ceil((count-160)/140)*0.3;
+                        }
+			$(obj).next().html('Characters: ' + count + '<br />Price: ' + price.toFixed(2) + ' kr/message');
 		};
 		
 		this.each(function() {  			

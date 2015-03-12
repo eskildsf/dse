5513656b7923a4284f8f2825a37bada046662845
Eskild Schroll-Fleischer <esf@studerende.dk>
2015-03-12 17:20:11 +0100

    Initial commit of qrstands This new app is a replacement for a quick and dirty attempt at a qrstand webapp. This new improved version provides logging of actions such as resetting a stand or declaring a stand empty.

---
+++ qrstand/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from qrstand.models import Stand, Log
+
+class StandAdmin(admin.ModelAdmin):
+    model = Stand
+    list_display = ('sid', 'top_scan_count', 'bottom_scan_count',)
+admin.site.register(Stand, StandAdmin)
+
+class LogAdmin(admin.ModelAdmin):
+    model = Log
+    list_display = ('stand', 'action',)
+    list_filter = ('stand', 'action',)
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields]
+    def has_add_permission(self, request, obj=None):
+        return False
+admin.site.register(Log, LogAdmin)
---
+++ qrstand/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+from django.db.models import F
+
+class Stand(models.Model):
+    sid = models.IntegerField(verbose_name='Stand Id')
+    top_scan_count = models.IntegerField(default=0, verbose_name='# scans in the top')
+    bottom_scan_count = models.IntegerField(default=0, verbose_name='# scans in the bottom')
+    def __unicode__(self):
+        return 'Stand %s' % self.sid
+    def reset(self, request):
+        self.top_scan_count = 0
+        self.bottom_scan_count = 0
+        self.save()
+        self.log(request, Log.RESET)
+    def empty(self, request, location):
+        if location == 'U':
+            self.bottom_scan_count=F('bottom_scan_count')+1
+        elif location == 'D':
+            self.top_scan_count = F('top_scan_count')+1
+        self.save()
+        self.log(request, location)
+    def log(self, request, type):
+        statement = Log(stand=self, action=type, ip=request.META['REMOTE_ADDR'])
+        statement.save()
+
+class Log(models.Model):
+    stand = models.ForeignKey(Stand)
+    created = models.DateTimeField(default=timezone.now,editable=False)
+    RESET = 'RE'
+    TOP_EMPTY = 'U'
+    BOTTOM_EMPTY = 'D'
+    ACTIONS = (
+        (RESET, 'Reset'),
+        (TOP_EMPTY, 'Top empty'),
+        (BOTTOM_EMPTY, 'Bottom empty'),
+    )
+    action = models.CharField(max_length=2, choices=ACTIONS)
+    ip = models.GenericIPAddressField()

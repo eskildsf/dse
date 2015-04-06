f765e4b5fe430747b2cdcbb98f1dbb1fa5954358
Eskild Schroll-Fleischer <esf@studerende.dk>
2015-04-06 14:46:31 +0200

    .

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ Personal projects for DSE. Information can be seen in the Wiki.
 
 To-do (humans):
 * Companies should be able to extract feedback from students attending their presentations (FAL)
-
-To-do (technical):
-* Put signal from studerende/__init__.py into its own file.
-* Catch up to Django 1.7
--- studerende/__init__.py
+++ studerende/__init__.py
@@ -1,10 +1,11 @@
-#from django.contrib.auth.models import User, Group
-#import django_auth_ldap.backend
+from django.contrib.auth.models import User, Group
+import django_auth_ldap.backend
+from datetime import datetime
 
-#def populate_profile(sender, user=None, ldap_user=None, **kwargs):
-#    Group.objects.get(name='DSE').user_set.add(user)
-#    user.is_active = True
-#    user.is_staff = True
-#    user.save()
+def populate_profile(sender, user=None, ldap_user=None, **kwargs):
+    Group.objects.get(name='DSE').user_set.add(user)
+    user.is_active = True
+    user.is_staff = True
+    user.save()
 
-#django_auth_ldap.backend.populate_user.connect(populate_profile)
+django_auth_ldap.backend.populate_user.connect(populate_profile)
